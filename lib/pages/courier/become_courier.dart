557c3fc53337f95e18c564b12c2c3a7e81d71437
REIAD ALARBI <Ryadel1999@gmail.com>
2023-05-14 22:27:16 +0300

    courier apply and uploading courier files

--- lib/pages/courier/become_courier.dart
+++ lib/pages/courier/become_courier.dart
@@ -9,7 +9,7 @@ class BecomeCourierPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Become a Courier'),
-        backgroundColor: Colors.teal, // Set the background color to teal
+        backgroundColor: Colors.teal,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -17,95 +17,81 @@ class BecomeCourierPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Why work with us?',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+              MyExpansionTile(
+                title: 'Why work with us?',
+                details: [
+                  'Flexible hours: Choose when and where you want to work. Whether you\'re looking for a full-time gig or just some extra cash on the side, we\'ve got you covered.',
+                  'Competitive pay: We offer competitive rates based on distance and delivery type.',
+                  'Easy to use app: Our user-friendly app makes it easy to manage your deliveries and track your earnings.',
+                  'Supportive team: Our team is always here to help, whether you have questions about the app or need assistance with a delivery.',
+                ],
+              ),
+              MyExpansionTile(
+                title: 'Requirements',
+                details: [
+                  'A reliable vehicle (car, truck, or van)',
+                  'A valid driver\'s license and insurance',
+                  'A smartphone with internet access',
+                  'Basic navigation skills',
+                  'A positive attitude and excellent customer service skills',
+                ],
+              ),
+              MyExpansionTile(
+                title: 'How to apply',
+                details: [
+                  'To apply to become a courier, simply fill out our online application form. We\'ll review your application and get back to you within 48 hours.',
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: MyButton2(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DocumentUploader(),
+                      ),
+                    );
+                  },
+                  buttonText: 'Continue',
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                '- Flexible hours: Choose when and where you want to work. Whether you\'re looking for a full-time gig or just some extra cash on the side, we\'ve got you covered.',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- Competitive pay: We offer competitive rates based on distance and delivery type.',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- Easy to use app: Our user-friendly app makes it easy to manage your deliveries and track your earnings.',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- Supportive team: Our team is always here to help, whether you have questions about the app or need assistance with a delivery.',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Requirements',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                '- A reliable vehicle (car, truck, or van)',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- A valid driver\'s license and insurance',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- A smartphone with internet access',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- Basic navigation skills',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '- A positive attitude and excellent customer service skills',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'How to apply',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'To apply to become a courier, simply fill out our online application form. We\'ll review your application and get back to you within 48 hours.',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              MyButton2(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DocumentUploader(),
-                    ),
-                  );
-                },
-                buttonText: 'Continue',
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyExpansionTile extends StatelessWidget {
+  final String title;
+  final List<String> details;
+
+  MyExpansionTile({required this.title, required this.details});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      tilePadding: EdgeInsets.all(0),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.teal,
+        ),
+      ),
+      children: details
+          .map((detail) => Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(
+                  detail,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ))
+          .toList(),
     );
   }
 }
--- lib/pages/courier/courier_register.dart
+++ lib/pages/courier/courier_register.dart
@@ -18,21 +18,6 @@ Future<void> addUserStatusPending(String userId) async {
       .update({"status": "pending"});
 }
 
-Future<String> getUserFullName(String uid) async {
-  String fullName = '';
-  try {
-    DocumentSnapshot userSnapshot =
-        await FirebaseFirestore.instance.collection('users').doc(uid).get();
-    Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
-    String firstName = userData['firstName'] as String;
-    String lastName = userData['lastName'] as String;
-    fullName = '$firstName $lastName';
-  } catch (error) {
-    print('Error getting user: $error');
-  }
-  return fullName;
-}
-
 class _DocumentUploaderState extends State<DocumentUploader> {
   File? _file1;
   File? _file2;
@@ -57,9 +42,9 @@ class _DocumentUploaderState extends State<DocumentUploader> {
     }
   }
 
-  void _uploadFiles() async {
+  void _uploadFiles(BuildContext context) async {
+    // Add BuildContext as a parameter
     if (_file1 != null && _file2 != null) {
-      // Get the current user's ID and retrieve first name and last name from Firestore
       final currentUser = FirebaseAuth.instance.currentUser;
       final uid = currentUser!.uid;
       final userDoc =
@@ -67,24 +52,25 @@ class _DocumentUploaderState extends State<DocumentUploader> {
       final firstName = userDoc.get("firstName");
       final lastName = userDoc.get("lastName");
 
-      // Create a new folder for the user if it doesn't already exist
       final folderRef = FirebaseStorage.instance
           .ref()
           .child("courier_records/$uid/$firstName" + "_" + "$lastName");
 
-      // Upload _file1 to Firebase Storage
       final file1Name = basename(_file1!.path);
       final file1Ref = folderRef.child(file1Name);
       await file1Ref.putFile(_file1!);
 
-      // Upload _file2 to Firebase Storage
       final file2Name = basename(_file2!.path);
       final file2Ref = folderRef.child(file2Name);
       await file2Ref.putFile(_file2!);
 
-      // Define uid before calling addUserStatusPending function
       final usid = currentUser!.uid;
       await addUserStatusPending(usid);
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => _buildDialogContent(context),
+      );
     }
   }
 
@@ -92,33 +78,145 @@ class _DocumentUploaderState extends State<DocumentUploader> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Upload Your Documents'),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ElevatedButton(
-              onPressed: _pickFile1,
-              child: Text("Upload Drivers License"),
+            Card(
+              elevation: 5,
+              child: ListTile(
+                leading: Icon(Icons.drive_file_rename_outline,
+                    color: Color(0xFFF57C00), size: 30),
+                title: Text('Drivers License',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                subtitle: _file1 == null
+                    ? Text("No file selected")
+                    : Text(basename(_file1!.path)),
+                trailing: IconButton(
+                  icon: Icon(Icons.file_upload, color: Color(0xFFF57C00)),
+                  onPressed: _pickFile1,
+                ),
+              ),
             ),
-            SizedBox(height: 16),
-            _file1 == null ? Text("No file selected") : Text(_file1!.path),
+            SizedBox(height: 20),
+            Card(
+              elevation: 5,
+              child: ListTile(
+                leading: Icon(Icons.drive_file_rename_outline,
+                    color: Color(0xFFF57C00), size: 30),
+                title: Text('Conviction Record',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                subtitle: _file2 == null
+                    ? Text("No file selected")
+                    : Text(basename(_file2!.path)),
+                trailing: IconButton(
+                  icon: Icon(Icons.file_upload, color: Color(0xFFF57C00)),
+                  onPressed: _pickFile2,
+                ),
+              ),
+            ),
             SizedBox(height: 32),
             ElevatedButton(
-              onPressed: _pickFile2,
-              child: Text("Upload Conviction Record"),
-            ),
-            SizedBox(height: 16),
-            _file2 == null ? Text("No file selected") : Text(_file2!.path),
-            SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: _uploadFiles,
-              child: Text("Upload Files"),
+              onPressed: () => _uploadFiles(context),
+              child: Text("Submit Documents"),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(16),
+                primary: Colors.teal,
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+Widget _buildDialogContent(BuildContext context) {
+  return Dialog(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20),
+    ),
+    elevation: 0,
+    backgroundColor: Colors.transparent,
+    child: Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            top: 100,
+            bottom: 20,
+            left: 20,
+            right: 20,
+          ),
+          margin: EdgeInsets.only(top: 45),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Thank You!',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Your application has been submitted. Please wait until the admin approves your request.',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 24.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).popUntil((route) => route.isFirst);
+                  },
+                  child: Text('Close'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 16,
+          right: 16,
+          child: CircleAvatar(
+            backgroundColor: Colors.green,
+            radius: 45,
+            child: Icon(
+              Icons.check,
+              color: Colors.white,
+              size: 50,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
